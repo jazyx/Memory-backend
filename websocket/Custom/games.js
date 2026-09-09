c320e8bf04141335360baf30d77500626dda09b6
James Newton <blackslate@lexogram.com>
2026-09-09 13:30:17 +0300

    LOGIN responds with both user_name and game_object

--- websocket/Custom/games.js
+++ websocket/Custom/games.js
@@ -13,6 +13,7 @@ const {
   getUserSocketsAndGroups,
   closeUserSockets
 } = require('../messageHub.js')
+const { getColor } = require('../Utilities/colors.js')
 
 
 treatMessageListener(
@@ -25,6 +26,20 @@ treatMessageListener(
 )
 
 
+const liveGames = new Map()
+
+
+function getGameObject () {
+  let game_object = liveGames.get("memory")
+  if (!game_object) {
+    game_object = require('./Games/memory.json')
+    liveGames.set("memory", game_object)
+  }
+
+  return game_object
+}
+
+
 async function logIn(incoming) {
   const {
     sender_id,
@@ -33,12 +48,35 @@ async function logIn(incoming) {
   } = incoming
   console.log("logIn:", incoming)
 
-  // TODO: Find or create game object, add user_name to players
-
-  const message =  {
+  let message =  {
     subject: "LOGGED_IN",
     recipient_id: sender_id,
-    user_name,
+    user_name
+  }
+  sendMessage(message)
+
+  const game_object = getGameObject()
+  const players = game_object.players
+  const exists = players.find(player => (
+    player.name.toLowerCase() === user_name.toLowerCase()
+  ))
+
+  if (!exists) {
+    // Add the new player
+    const number = players.length
+    const color = getColor({number})
+    players.push({
+      name: user_name,
+      score: 0,
+      color
+    })
+}
+  console.log("game_:", game_object)
+
+  message =  {
+    subject: "GAME_OBJECT",
+    recipient_id: sender_id,
+    game_object,
     uNiQiD
   }
   sendMessage(message)
